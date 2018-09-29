5a703b71a067f7860122796d3bcb784a8f3bfca3
Tomasz Rybarczyk <paluho@gmail.com>
2018-09-29 19:00:37 +0200

    Add tests

---
+++ test/Main.purs
@@ -0,0 +1,87 @@
+module Test.Main where
+
+import Prelude
+
+import Control.Comonad (extend, extract)
+import Data.Array (fromFoldable, singleton, snoc) as Array
+import Data.Foldable (fold, foldMap, sum)
+import Data.List (length) as List
+import Data.List.Pointed (Pointed(..), insertLeft, insertRight, prev)
+import Data.List.Pointed (fromFoldable) as Pointed
+import Data.Maybe (Maybe(..))
+import Data.Monoid.Additive (Additive(..))
+import Effect (Effect)
+import Test.Unit (suite, test)
+import Test.Unit.Assert (equal)
+import Test.Unit.Main (runTest)
+
+main :: Effect Unit
+main = runTest $ do
+  suite "Data.List.Pointed" $ do
+    test "fromFoldable" do
+      let arr = [1,2,3]
+      equal (Array.fromFoldable <$> Pointed.fromFoldable arr) (Just arr)
+    test "foldMap" do
+      let arr = [1, 2, 3]
+      equal (foldMap Array.singleton <$> Pointed.fromFoldable arr) (Just arr)
+    test "fold" do
+      let arr = [Additive 1, Additive 2, Additive 3]
+      equal (fold <$> Pointed.fromFoldable arr) (Just (Additive 6))
+    test "insertRight" do
+      let
+        arr = [1, 2, 3]
+        new = 4
+        p = insertRight new <$> Pointed.fromFoldable arr
+      equal (Array.fromFoldable <$> p) (Just (Array.snoc arr new))
+      equal (extract <$> p) (Just new)
+    test "insertLeft" do
+      let
+        arr = [1, 2, 3]
+        new = 4
+        p = insertLeft new <$> Pointed.fromFoldable arr
+      equal (Array.fromFoldable <$> p) (Just [1,2,4,3])
+      equal (extract <$> p) (Just new)
+    test "extend extract from the end" do
+      let
+        arr = [1, 2, 3]
+        initial = Pointed.fromFoldable arr
+        p = extend extract <$> initial
+      equal (Array.fromFoldable <$> initial) (Array.fromFoldable <$> p)
+      equal (extract <$> initial) (extract <$> p)
+    test "extend extract from the beginning" do
+      let
+        arr = [1, 2, 3]
+        initial = Pointed.fromFoldable arr >>= prev >>= prev
+        p = extend extract <$> initial
+      equal (Array.fromFoldable <$> initial) (Array.fromFoldable <$> p)
+      equal (extract <$> initial) (extract <$> p)
+    test "extend assoc end" do
+      let
+        arr = [1, 2, 3, 4]
+        pos (Pointed { reversedPrefix }) = List.length reversedPrefix
+        sum' (Pointed { suffix }) = sum suffix
+        initial = Pointed.fromFoldable arr
+        p = extend sum' <<< extend sum' <$> initial
+        p' =  extend (sum' <<< extend sum') <$> initial
+      equal (Array.fromFoldable <$> p') (Array.fromFoldable <$> p)
+      equal (extract <$> p') (extract <$> p)
+    test "extend assoc" do
+      let
+        arr = [1, 2, 3, 4]
+        pos (Pointed { reversedPrefix }) = List.length reversedPrefix
+        sum' (Pointed { suffix }) = sum suffix
+        initial = Pointed.fromFoldable arr >>= prev
+        p = extend sum' <<< extend sum' <$> initial
+        p' =  extend (sum' <<< extend sum') <$> initial
+      equal (Array.fromFoldable <$> p') (Array.fromFoldable <$> p)
+      equal (extract <$> p') (extract <$> p)
+    test "extend assoc start" do
+      let
+        arr = [1, 2, 3, 4]
+        pos (Pointed { reversedPrefix }) = List.length reversedPrefix
+        sum' (Pointed { suffix }) = sum suffix
+        initial = Pointed.fromFoldable arr >>= prev >>= prev
+        p = extend sum' <<< extend sum' <$> initial
+        p' =  extend (sum' <<< extend sum') <$> initial
+      equal (Array.fromFoldable <$> p') (Array.fromFoldable <$> p)
+      equal (extract <$> p') (extract <$> p)
